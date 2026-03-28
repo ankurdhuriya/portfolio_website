c40d4a4fb49f3a48493c014d908320ed98d2aa62
ankurdhuriya <mail.ankurdhuriya@gmail.com>
2026-03-28 13:00:38 +0530

    fix: scroll to top added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CaseStudyPage from './pages/CaseStudyPage';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/work/:slug" element={<CaseStudyPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/work/:slug" element={<CaseStudyPage />} />
+      </Routes>
+    </>
   );
 }
 
